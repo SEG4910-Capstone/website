fc6ffe70a739ced5e9b13a161104335445acf516
Azan Mubashar <71412049+azan-m@users.noreply.github.com>
2024-10-04 01:16:02 -0400

    bug fix

---
+++ uoav-website/src/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { FaInstagram, FaYoutube, FaLinkedin } from 'react-icons/fa';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer-container">
+      <h2 className="footer-title">uOAV</h2>
+      <p>Join the Team | Become a Partner</p>
+      <div className="footer-icons">
+        <FaInstagram className="footer-icon" />
+        <FaYoutube className="footer-icon" />
+        <FaLinkedin className="footer-icon" />
+      </div>
+      <p>© 2021-2023 uOAV | Powered by uOAV</p>
+    </footer>
+  );
+};
+
+export default Footer;
