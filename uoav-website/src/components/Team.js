038836f854e71f6861f5fe3c2f6fa9c70cecb865
Azan Mubashar <71412049+azan-m@users.noreply.github.com>
2024-10-23 02:02:14 -0400

    Added teams section

---
+++ uoav-website/src/components/Team.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../styles/Team.css';
+
+const Team = () => {
+  return (
+    <section className="team-section">
+      <div className="team-background">
+        <div className="team-overlay">
+          <div className="team-content">
+            <h2 className="team-title">Our Team</h2>
+            <p className="team-description">
+              Maecenas pharetra justo et nunc aliquam dignissim. Nunc consequat nunc vel metus malesuada ullamcorper. Praesent sed nisl diam. Nulla vitae sapien vitae lacus iaculis sagittis. Ut gravida lacinia iaculis. Cras sollicitudin condimentum neque vitae ultricies. Nulla tempus sapien mi, a consequat velit fermentum at.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Team;
