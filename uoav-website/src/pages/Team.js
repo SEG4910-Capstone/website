afff593a279c06ba5e8a1784b64275f977a5863d
Azan Mubashar <71412049+azan-m@users.noreply.github.com>
2024-11-26 02:17:31 -0500

    updated images and banners

--- uoav-website/src/pages/Team.js
+++ uoav-website/src/pages/Team.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import '../styles/Team.css';
-import electrical from '../electrical.jpg';
-import mechanical from '../mechanical.jpg';
+import electrical from '../electrical cartoon.png';
+import mechanical from '../mechanical cartoon.png';
+import software from '../software cartoon.png';
+import marketing from '../marketing cartoon.png';
 
 const Team = () => {
   return (
-    <div className="team-page">
-    
-        <div className="overlay">
-          <section className="partners-header text-center py-5">
+    <><div className="team-page">
+      <section className="partners-header text-center py-5">
         <h1 className="section-title">Our Team</h1>
       </section>
-        </div>
-
-
       <section className="division mechanical">
         <div className="text">
           <h2>Mechanical</h2>
@@ -37,20 +34,25 @@ const Team = () => {
           <h2>Software</h2>
           <p>By joining this division, you will gain hands-on experience in the rapidly growing field of autonomous robotics while contributing your skills to the creation of sophisticated, self-driving machines that will revolutionize the future of transportation and robotics.</p>
         </div>
-        <div className="media-placeholder"></div>
+        <div className="media-placeholder">
+          <img src={software} alt="Software Team" />
+        </div>
       </section>
       <section className="division marketing">
-        <div className="media-placeholder"></div>
+      <div className="media-placeholder">
+          <img src={marketing} alt="Marketing Team" />
+        </div>
         <div className="text">
           <h2>Marketing</h2>
           <p>In the Marketing division, you will be responsible for building a strong online presence for the team and promoting projects. Your contributions will ensure the team’s efforts are well-documented and showcased effectively to stakeholders, sponsors, and the public.</p>
         </div>
       </section>
-      <footer className="cta-footer">
-        <h2>Thinking of Joining?</h2>
-        <button className="cta-button">Join the Team →</button>
-      </footer>
-    </div>
+    </div><div>
+        <footer className="cta-footer">
+          <h2>Thinking of Joining?</h2>
+          <button className="cta-button">Join the Team →</button>
+        </footer>
+      </div></>
   );
 };
 
